b56df0a196d17ac3a994955f2b45239bf5854007
Luis Arias <larias@localhost.localdomain>
2021-02-08 22:30:51 -0300

    Primer commit, pequeño ejercicio para ver VUEX

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,35 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        msg: 'Vuex desde cero',
+        nombre: 'Luis',
+        apellido: 'Arias',
+        amigos: []
+    },
+    mutations: {
+        addAmigo(state, amigo) {
+            state.amigos = [amigo, ...state.amigos];
+        },
+        delAmigo(state, indice) {
+            state.amigos.splice(indice, 1);
+        }
+    },
+    actions: {
+        addAmigoAction(context, amigo) {
+            context.commit('addAmigo', amigo);
+        },
+        delAmigoAction(context, indice) {
+            context.commit('delAmigo', indice);
+        }
+    },
+    modules: {},
+    getters: {
+        mensaje(state) {
+            return state.msg;
+        },
+        nombreCompleto(state) {
+            return `${state.nombre} ${state.apellido}`
+        }
+    }
 })
